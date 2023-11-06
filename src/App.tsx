66a1f60b738cdf8ce6499f6a3476970216ee22a8
carrynong2 <carrynong2@gmail.com>
2023-11-06 08:43:46 +0700

    add VideoGridItem component and formatTime

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import CategoryPills from './component/CategoryPills'
 import './index.css'
 import { PageHeader } from './layouts/PageHeader'
-import { categories } from './data/home'
+import { categories, videos } from './data/home'
 import { useState } from 'react'
+import VideoGridItem from './component/VideoGridItem'
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   return (
-    <div className='max-h-screen flex flex-col'>
+    <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div className='grid grid-cols-[auto, 1fr] flex-grow-1 overflow-auto'>
-        <div>Sidebar</div>
-        <div className='overflow-x-hidden'>
-          <div className='sticky top-0 bg-white z-10 pb-4 px-8'>
-            <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map(video => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
