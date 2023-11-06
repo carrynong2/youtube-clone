665da947876cab54a3dc6710db077db5300807a7
carrynong2 <carrynong2@gmail.com>
2023-11-06 08:53:38 +0700

    add animation for VideoGridItem component

--- src/component/VideoGridItem.tsx
+++ src/component/VideoGridItem.tsx
@@ -38,12 +38,15 @@ const VideoGridItem = ({
   }, [isVideoPlaying])
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2"
+      onMouseEnter={() => setIsVideoPlaying(true)}
+      onMouseLeave={() => setIsVideoPlaying(false)}>
       <a href={`/watch?v=${id}`} className="relative aspect-video">
-        <img src={thumbnailUrl} className="block w-full h-full object-cover rounded-xl" />
+        <img src={thumbnailUrl} className={`block w-full h-full object-cover transition-[border-radius] duration-200 ${isVideoPlaying ? "rounded-none" : "rounded-xl"}`} />
         <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
           {formatDuration(duration)}
         </div>
+        <video className={`block h-full object-cover absolute inset-0 translate-opacity duration-200 ${isVideoPlaying ? "opacity-100 delay-200" : "opacity-0"}`} ref={videoRef} muted playsInline src={videoUrl} />
       </a>
       <div className="flex gap-2">
         <a href={`/@${channel.id}`} className="flex-shrink-0">
@@ -57,7 +60,7 @@ const VideoGridItem = ({
           {VIEW_FORMATTER.format(views)} Views • {formatTimeAgo(postedAt)}
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
